aa413925fb3eb598db7c634fb462a8c2bd1c4f15
Cesar Dominguez <FIBEX@FIBEX-867.fibex.local>
2025-07-22 12:58:04 -0400

    Agregando Tangram

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError, RouterOutlet } from '@angular/router';
+import { LoadingService } from './loading.service';
+import { TangramLoadingComponent } from './components/shared/tangram-loading/tangram-loading.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TangramLoadingComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  loading$;
+
+  constructor(private router: Router, private loadingService: LoadingService) {
+    this.loading$ = this.loadingService.loading$;
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.loadingService.show();
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        setTimeout(() => this.loadingService.hide(), 400);
+      }
+    });
+  }
   protected title = 'client';
 }
